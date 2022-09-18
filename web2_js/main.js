400c7d69654a64410be34c0eeb1c536601b0e6a8
DongJu Kim <gggg8657@gmail.com>
2022-09-19 00:58:40 +0900

    post 로 전송된 데이터를 받는 코드 작성

--- web2_js/main.js
+++ web2_js/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
   return `
@@ -13,6 +14,7 @@ function templateHTML(title, list, body){
   <body>
   <h1><a href="/">WEB</a></h1>
   ${list}
+  <a href="/create">create</a>
   ${body}
   </body>
   </html>
@@ -34,12 +36,12 @@ var app = http.createServer(function(request,response){
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
 
-  console.log(url.parse(_url, true).pathname);
-
+  //console.log(url.parse(_url, true).pathname);
+  //console.log(pathname);
   if(pathname === '/'){ //홈 과 페이지를 구분 할 수 없다.
     if(queryData.id===undefined){
       fs.readdir('./data', function(error, filelist){
-        console.log(filelist);
+        //console.log(filelist);
         var title = 'WELCOME';
         var description = 'Hello, Node.js';
         var list=templateLIST(filelist);
@@ -51,7 +53,7 @@ var app = http.createServer(function(request,response){
 
     else {
       fs.readdir('./data', function(error, filelist){
-        console.log(filelist);
+        //console.log(filelist);
         fs.readFile(`DATA/${queryData.id}`, 'utf8',
         function(err, description){
           var title = queryData.id;
@@ -63,9 +65,52 @@ var app = http.createServer(function(request,response){
       });
     }
   }
-  else{
-    response.writeHead(404);
-    response.end("Not found");
-  }
-});
-app.listen(3000);
+  else if(pathname === '/create'){
+    fs.readdir('./data', function(error, filelist){
+      //console.log(filelist);
+      var title = 'WEB - create';
+      var list=templateLIST(filelist);
+      var template = templateHTML(title, list, `
+        <form action="http://localhost:3000/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+        </form>
+        `);
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+    // post 방식으로 전송된 데이터를 node js 로 가져오는 방법은 뭘까? -> 이해 하지 말고 그냥 익숙하게 가자.
+    else if(pathname ==='/create_process')
+    {
+      var body = '';
+      //node js 에서는 post 방식을 전송된 데이터가 많을 경우에 어떤 특정한 양만큼을 수신할 때마다, 서버는 callback 함수를 호출하도록 약속되어있다.
+      //수신한 정보를 주는것.
+      request.on('data', function(data){
+        body = body + data;
+          //이 콜백 함수가 실행 될 때마다 데이터를 body 에 추가하는 기능
+      });
+      request.on('end', function(){
+        //end 에 해당되는 콜백인 fucntio() 이 실행되면?
+        var post = qs.parse(body);
+        //post 에 정보 넣기.
+        console.log(post);
+
+        var title = post.title;
+        var description = post.description;
+        console.log(`${title} , ${description}`);
+      });
+      response.writeHead(200);
+      response.end("success");
+    }
+    else {
+      response.writeHead(404);
+      response.end("Not found");
+    }
+  });
+  app.listen(3000);
