44d15f6fb6c8ba28eef2800a4585ab88597d7b15
DongJu Kim <gggg8657@gmail.com>
2022-09-18 18:51:59 +0900

    using function

---
+++ syntax/function.js
@@ -0,0 +1,18 @@
+f123();
+console.log('a');
+console.log('z');
+console.log('b');
+f123();
+console.log('f');
+console.log('t');
+console.log('감');
+f123();
+console.log('djkl');
+f123();
+
+function f123(){
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+}
---
+++ syntax/function2.js
@@ -0,0 +1,11 @@
+console.log(Math.round(1.4));//1
+console.log(Math.round(1.6));//2
+
+function sum(first, second){//parameter
+  console.log(first+second);
+  console.log(`${first} + ${second} = ${first+second}`);
+
+
+}
+
+sum(2,4); //argument
