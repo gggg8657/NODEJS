085f4ed19e495a73ab80962e882ef59d738247e0
DongJu Kim <gggg8657@gmail.com>
2022-09-19 13:48:57 +0900

    C (DONE) R (NOT YET) U (NOT YET) D (NOT YET)

--- web2_js/main.js
+++ web2_js/main.js
@@ -103,10 +103,13 @@ var app = http.createServer(function(request,response){
 
         var title = post.title;
         var description = post.description;
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {Location:`/?id=${title}`});//302 is redirection the site
+          response.end("success");
+        })
         console.log(`${title} , ${description}`);
       });
-      response.writeHead(200);
-      response.end("success");
+
     }
     else {
       response.writeHead(404);
