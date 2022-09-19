8070d60b5b6637f86e2d80704fd4bd3b4ddde376
DongJu Kim <gggg8657@gmail.com>
2022-09-19 14:26:01 +0900

    U (on going)

--- web2_js/main.js
+++ web2_js/main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
   <body>
   <h1><a href="/">WEB</a></h1>
   ${list}
-  <a href="/create">create</a>
+  ${control}
   ${body}
   </body>
   </html>
@@ -45,7 +45,10 @@ var app = http.createServer(function(request,response){
         var title = 'WELCOME';
         var description = 'Hello, Node.js';
         var list=templateLIST(filelist);
-        var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+        var template = templateHTML(title, list,
+          `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`
+        );
         response.writeHead(200);
         response.end(template);
       });
@@ -58,7 +61,10 @@ var app = http.createServer(function(request,response){
         function(err, description){
           var title = queryData.id;
           var list=templateLIST(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+          var template = templateHTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          );
           response.writeHead(200);
           response.end(template);
         });
@@ -71,16 +77,16 @@ var app = http.createServer(function(request,response){
       var title = 'WEB - create';
       var list=templateLIST(filelist);
       var template = templateHTML(title, list, `
-        <form action="http://localhost:3000/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
+        <form action="/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+        <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+        <input type="submit">
+        </p>
         </form>
-        `);
+        `, '');
         response.writeHead(200);
         response.end(template);
       });
@@ -93,7 +99,7 @@ var app = http.createServer(function(request,response){
       //수신한 정보를 주는것.
       request.on('data', function(data){
         body = body + data;
-          //이 콜백 함수가 실행 될 때마다 데이터를 body 에 추가하는 기능
+        //이 콜백 함수가 실행 될 때마다 데이터를 body 에 추가하는 기능
       });
       request.on('end', function(){
         //end 에 해당되는 콜백인 fucntio() 이 실행되면?
@@ -103,14 +109,43 @@ var app = http.createServer(function(request,response){
 
         var title = post.title;
         var description = post.description;
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        fs.writeFile(`DATA/${title}`, description, 'utf8', function(err){
           response.writeHead(302, {Location:`/?id=${title}`});//302 is redirection the site
-          response.end("success");
+          response.end();
         })
         console.log(`${title} , ${description}`);
       });
-
     }
+    else if (pathname==='/update'){
+      fs.readdir('./DATA', function(error, filelist){
+        //console.log(filelist);
+        fs.readFile(`DATA/${queryData.id}`, 'utf8',
+        function(err, description){
+          var title = queryData.id;
+          var list=templateLIST(filelist);
+          var template = templateHTML(title, list,
+            //`<h2>${title}</h2>${description}`,
+            `
+            <form action="/update_proces" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+            <input type="submit">
+            </p>
+            </form>
+
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          );
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    }
+
     else {
       response.writeHead(404);
       response.end("Not found");
